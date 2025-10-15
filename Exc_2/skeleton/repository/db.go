fa41879452253dd99d085c516a65423092d395fe
mariahelmi <s2410929013@fhooe.at>
2025-10-15 17:33:14 +0200

    Exercise 2

--- Exc_2/skeleton/repository/db.go
+++ Exc_2/skeleton/repository/db.go
@@ -1,6 +1,9 @@
 package repository
 
-import "ordersystem/model"
+import (
+	"ordersystem/model"
+	"time"
+)
 
 type DatabaseHandler struct {
 	// drinks represent all available drinks
@@ -12,14 +15,23 @@ type DatabaseHandler struct {
 // todo
 func NewDatabaseHandler() *DatabaseHandler {
 	// Init the drinks slice with some test data
-	// drinks := ...
-
+	drinks := []model.Drink{
+		{ID: 1, Name: "Espresso", Price: 2.5, Description: "Strong and bold coffee shot"},
+		{ID: 2, Name: "Cappuccino", Price: 3.0, Description: "Espresso with steamed milk and foam"},
+		{ID: 3, Name: "Iced Latte", Price: 3.5, Description: "Chilled coffee with milk and ice"},
+	}
 	// Init orders slice with some test data
+	orders := []model.Order{
+		{DrinkID: 1, CreatedAt: time.Now().Add(-2 * time.Hour), Amount: 2},
+		{DrinkID: 2, CreatedAt: time.Now().Add(-1 * time.Hour), Amount: 1},
+		{DrinkID: 3, CreatedAt: time.Now(), Amount: 3},
+	}
 
 	return &DatabaseHandler{
 		drinks: drinks,
 		orders: orders,
 	}
+
 }
 
 func (db *DatabaseHandler) GetDrinks() []model.Drink {
@@ -32,6 +44,14 @@ func (db *DatabaseHandler) GetOrders() []model.Order {
 
 // todo
 func (db *DatabaseHandler) GetTotalledOrders() map[uint64]uint64 {
+	totalledOrders := make(map[uint64]uint64)
+	for i := 0; i < len(db.orders); i++ {
+		current_order := db.orders[i]
+
+		key := uint64(current_order.DrinkID)
+		value := uint64(current_order.Amount)
+		totalledOrders[key] += value
+	}
 	// calculate total orders
 	// key = DrinkID, value = Amount of orders
 	// totalledOrders map[uint64]uint64
@@ -39,6 +59,8 @@ func (db *DatabaseHandler) GetTotalledOrders() map[uint64]uint64 {
 }
 
 func (db *DatabaseHandler) AddOrder(order *model.Order) {
+	db.orders = append(db.orders, *order)
 	// todo
 	// add order to db.orders slice
+
 }
--- Exc_2/skeleton/rest/api.go
+++ Exc_2/skeleton/rest/api.go
@@ -1,7 +1,9 @@
 package rest
 
 import (
+	"encoding/json"
 	"net/http"
+	"ordersystem/model"
 	"ordersystem/repository"
 
 	"github.com/go-chi/render"
@@ -15,6 +17,9 @@ import (
 // @Router 			/api/menu [get]
 func GetMenu(db *repository.DatabaseHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		Menu := db.GetDrinks()
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r, Menu)
 		// todo
 		// get slice from db
 		// render.Status(r, http.StatusOK)
@@ -23,8 +28,22 @@ func GetMenu(db *repository.DatabaseHandler) http.HandlerFunc {
 }
 
 // todo create GetOrders /api/order/all
+func GetOrders(db *repository.DatabaseHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		Orders := db.GetOrders()
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r, Orders)
+	}
+}
 
 // todo create GetOrdersTotal /api/order/total
+func GetOrdersTotal(db *repository.DatabaseHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		Totalorders := db.GetTotalledOrders()
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r, Totalorders)
+	}
+}
 
 // PostOrder 		godoc
 // @tags 			Order
@@ -37,12 +56,21 @@ func GetMenu(db *repository.DatabaseHandler) http.HandlerFunc {
 // @Router 			/api/order [post]
 func PostOrder(db *repository.DatabaseHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		order := model.Order{}
+		err := json.NewDecoder(r.Body).Decode(&order)
+		if err != nil {
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, err.Error())
+		} else {
+			db.AddOrder(&order)
+			render.Status(r, http.StatusOK)
+			render.JSON(w, r, "ok")
+		}
 		// todo
 		// declare empty order struct
 		// err := json.NewDecoder(r.Body).Decode(&<your-order-struct>)
 		// handle error and render Status 400
 		// add to db
-		render.Status(r, http.StatusOK)
-		render.JSON(w, r, "ok")
+
 	}
 }
