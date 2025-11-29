d8a919540e7f647e28fbcc88b60badaf6c84b1d1
mariahelmi <s2410929013@fhooe.at>
2025-11-29 15:01:59 +0100

    Exercise8_finished

--- Exc_8/skeleton/client/grpc_client.go
+++ Exc_8/skeleton/client/grpc_client.go
@@ -15,7 +15,7 @@ type GrpcClient struct {
 }
 
 func NewGrpcClient() (*GrpcClient, error) {
-	conn, err := grpc.NewClient(":4000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(":4000", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
@@ -24,12 +24,62 @@ func NewGrpcClient() (*GrpcClient, error) {
 }
 
 func (c *GrpcClient) Run() error {
+	ctx := context.Background()
 	// todo
 	// 1. List drinks
+	fmt.Println("Requesting drinks 🍹🍺☕")
+	drinksResp, err := c.client.GetDrinks(ctx, &emptypb.Empty{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Available drinks:")
+	for _, d := range drinksResp.Drinks {
+		fmt.Printf("\t> id:%d  name:%q  price:%d  description:%q\n",
+			d.Id, d.Name, d.Price, d.Desc)
+	}
+
 	// 2. Order a few drinks
+	fmt.Println("Ordering drinks 👨‍🍳⏱️🍻🍻")
+	for _, d := range drinksResp.Drinks {
+		fmt.Printf("\t> Ordering: 2 x %s\n", d.Name)
+		_, err = c.client.OrderDrink(ctx, &pb.OrderRequest{
+			DrinkId: d.Id,
+			Amount:  2,
+		})
+		if err != nil {
+			return err
+		}
+	}
 	// 3. Order more drinks
-	// 4. Get order total
-	//
+	fmt.Println("Ordering another round of drinks 👨‍🍳⏱️🍻🍻")
+	for _, d := range drinksResp.Drinks {
+		fmt.Printf("\t> Ordering: 6 x %s\n", d.Name)
+		_, err = c.client.OrderDrink(ctx, &pb.OrderRequest{
+			DrinkId: d.Id,
+			Amount:  6,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	fmt.Println("Getting the bill 💹💹💹")
+	ordersResp, err := c.client.GetOrders(ctx, &emptypb.Empty{})
+	if err != nil {
+		return err
+	}
+
+	// Aggregate totals
+	totals := map[uint64]uint64{}
+	for _, o := range ordersResp.Orders {
+		totals[o.DrinkId] += o.Amount
+	}
+
+	for _, d := range drinksResp.Drinks {
+		fmt.Printf("\t> Total: %d x %s\n", totals[d.Id], d.Name)
+	}
+
+	fmt.Println("Orders complete!")
 	// print responses after each call
 	return nil
 }
--- Exc_8/skeleton/server/grpc_server.go
+++ Exc_8/skeleton/server/grpc_server.go
@@ -3,24 +3,34 @@ package server
 import (
 	"context"
 	"exc8/pb"
-	"fmt"
-	"log/slog"
+	// "fmt"
+	// "log/slog"
 	"net"
 
 	"google.golang.org/grpc"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
-	wrapperspb "google.golang.org/protobuf/types/known/wrapperspb"
+	// wrapperspb "google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 type GRPCService struct {
 	pb.UnimplementedOrderServiceServer
+
+	drinks []*pb.Drink
+	orders []*pb.Order
 }
 
 func StartGrpcServer() error {
 	// Create a new gRPC server.
 	srv := grpc.NewServer()
 	// Create grpc service
-	grpcService := &GRPCService{}
+	grpcService := &GRPCService{
+		drinks: []*pb.Drink{
+            {Id: 1, Name: "Spritzer", Desc: "Wine with soda", Price: 2},
+            {Id: 2, Name: "Beer", Desc: "Hagenberger Gold", Price: 3},
+            {Id: 3, Name: "Coffee", Desc: "Mifare isn't that secure", Price: 0},
+        },
+        orders: []*pb.Order{},
+    }
 	// Register our service implementation with the gRPC server.
 	pb.RegisterOrderServiceServer(srv, grpcService)
 	// Serve gRPC server on port 4000.
@@ -28,11 +38,25 @@ func StartGrpcServer() error {
 	if err != nil {
 		return err
 	}
-	err = srv.Serve(lis)
-	if err != nil {
-		return err
-	}
-	return nil
+	return srv.Serve(lis)
 }
 
 // todo implement functions
+
+func (s *GRPCService) GetDrinks(ctx context.Context, _ *emptypb.Empty) (*pb.GetDrinksResponse, error) {
+	return &pb.GetDrinksResponse{Drinks: s.drinks}, nil
+}
+
+func (s *GRPCService) GetOrders(ctx context.Context, _ *emptypb.Empty) (*pb.GetOrdersResponse, error) {
+	return &pb.GetOrdersResponse{Orders: s.orders}, nil
+}
+
+func (s *GRPCService) OrderDrink(ctx context.Context, req *pb.OrderRequest) (*pb.OrderResponse, error) {
+	order := &pb.Order{
+		DrinkId: req.DrinkId,
+		Amount:  req.Amount,
+	}
+	s.orders = append(s.orders, order)
+
+	return &pb.OrderResponse{Order: order}, nil
+}
